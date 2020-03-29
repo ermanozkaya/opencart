588b1e1edf3c10cbaf2399735a758643c7a66acc
teknovipyazilim <37733016+teknovipyazilim@users.noreply.github.com>
2020-03-29 23:14:15 +0300

    Update teknopayment.php

--- teknopayment/upload/admin/language/en-gb/extension/payment/teknopayment.php
+++ teknopayment/upload/admin/language/en-gb/extension/payment/teknopayment.php
@@ -1,13 +1,12 @@
 <?php
 // Heading
 $_['heading_title']      = 'Multiple Payment Module';
-
 // Text
 $_['text_extension']     = 'Extensions';
+$_['text_teknopayment']     = '<img src="'.HTTP_CATALOG.'image/catalog/payment/tulparstudyo.png" height=32>';
 $_['text_success']       = 'Success: You have modified Multiple Payment Module!';
 $_['text_edit']          = 'Edit Multiple Payment Module';
 $_['text_add_new_payment_type'] = 'Add New Payment Type';
-
 // Entry
 $_['entry_title']        = 'Payment Title';
 $_['entry_description']  = 'Payment Description';
@@ -16,9 +15,7 @@ $_['entry_order_status'] = 'Order Status';
 $_['entry_geo_zone']     = 'Geo Zone';
 $_['entry_status']       = 'Status';
 $_['entry_sort_order']   = 'Sort Order';
-
 // Help
 $_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
-
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify payment Multiple Payment Module!';
